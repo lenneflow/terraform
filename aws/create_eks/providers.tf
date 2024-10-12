7675dd4663b7f3ffdc663c01e2ac584c36f7e8c2
Idrissa GANEMTORE <dev@ganemtore.net>
2024-10-12 13:55:41 -0400

    minor

--- aws/create_eks/providers.tf
+++ aws/create_eks/providers.tf
@@ -41,7 +41,16 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.auth.token
+    exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
+  }
 }
 
 # Helm Provider
@@ -49,6 +58,15 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.auth.token
+      exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.cluster_name
+    ]
+  }
   }
 }
