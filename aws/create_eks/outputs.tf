ee39deee7a9af10a2b69b50e4539302134387705
Idrissa GANEMTORE <dev@ganemtore.net>
2024-10-12 17:52:22 -0400

    update

--- aws/create_eks/outputs.tf
+++ aws/create_eks/outputs.tf
@@ -3,6 +3,11 @@ output "cluster_endpoint" {
   value       = module.eks.cluster_endpoint
 }
 
+output "cluster_ca_certificate" {
+  description = "cluster_certificate"
+  value       = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
   value       = module.eks.cluster_security_group_id
