d1f76d23bc6f97056affa1ed51b0d3078bb815fa
Idrissa GANEMTORE <dev@ganemtore.net>
2024-10-12 14:02:28 -0400

    minor

--- aws/create_eks/nginx_ingress.tf
+++ aws/create_eks/nginx_ingress.tf
@@ -3,7 +3,7 @@ resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "4.0.6"
+  version    = "4.11.2"
   namespace  = "kube-system"
 
   values = [
