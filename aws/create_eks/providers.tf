bf285a54e310256a507b52ccd2b4f706fe081392
Idrissa GANEMTORE <dev@ganemtore.net>
2024-10-14 18:25:55 -0400

    update

--- aws/create_eks/providers.tf
+++ aws/create_eks/providers.tf
@@ -39,36 +39,32 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+/*
+Retrieve information about an EKS Cluster.
+*/
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+/*
+Get an authentication token to communicate with an EKS cluster.
+*/
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
 # Kubernetes Provider
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      var.cluster_name
-    ]
-  }
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 # Helm Provider
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks",
-      "get-token",
-      "--cluster-name",
-      var.cluster_name
-    ]
-  }
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
