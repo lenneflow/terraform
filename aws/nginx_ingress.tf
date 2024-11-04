e6c8b04635b63c23b527359a05da53837ad9d744
Idrissa GANEMTORE <dev@ganemtore.net>
2024-11-04 18:49:13 -0500

    Added metrics server resource

--- aws/nginx_ingress.tf
+++ aws/nginx_ingress.tf
@@ -19,3 +19,17 @@ data "kubernetes_service" "nginx_ingress" {
   }
 }
 
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  depends_on = [module.eks ]
+  namespace  = "metrics-server"
+  create_namespace = true
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls=true}"
+  }
+  wait = true
+}
+
